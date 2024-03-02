5feb82e2f2ec56adb1c18c38f2b5f6f03a811fef
Sergey Kazakov <kazaimazai@gmail.com>
2024-03-02 17:58:41 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftyModel
+
+A description of this package.
---
+++ Tests/SwiftyModelTests/SwiftyModelTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftyModel
+
+final class SwiftyModelTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftyModel().text, "Hello, World!")
+    }
+}
