a0ae22091419db234ed091a808e6b903f04a98e0
Sergey Kazakov <kazaimazai@gmail.com>
2024-03-03 00:47:46 +0000

    test models

--- Tests/SwiftyModelTests/SwiftyModelTests.swift
+++ Tests/SwiftyModelTests/SwiftyModelTests.swift
@@ -9,53 +9,24 @@ final class SwiftyModelTests: XCTestCase {
         
     }
     
-    
-    struct User: IdentifiableEntity, Codable {
-        
-        let id: String
-        let name: String
-        var messages: [Relation<Message>]?
-        var follows: [MutualRelation<User>]?
-        var followedBy: [MutualRelation<User>]?
-        
-        mutating func normalize() {
-            messages?.normalize()
-            follows?.normalize()
-        }
-    }
-
-    struct Message: IdentifiableEntity, Codable {
-        let id: String
-        let text: String
-        var attachments: [MutualRelation<Attachment>]?
-        
-        mutating func normalize() {
-            attachments?.normalize()
-        }
-    }
-
-    struct Attachment: IdentifiableEntity, Codable {
-        let id: String
-        let link: String
-        var message: MutualRelation<Message>?
-        
-        mutating func normalize() {
-            message?.normalize()
-        }
-    }
 
 
     func test() {
-        let attachment = Attachment(id: "1", link: "")
+        let attachment = Attachment(id: "1", url: "http://google.com")
         let message = Message(id: "1", text: "the message", attachments: [MutualRelation(attachment)])
         
         var storage = Repository()
         let user = User(id: "2", name: "alice", messages: [Relation(message)])
         
+        let currentUser = CurrentUser(user: MutualRelation(user))
         
         storage.save(user)
         storage.save(user.follows)
         storage.save(user.relation(\.follows, option: .append, inverse: \.followedBy))
+        
+        storage.save(currentUser)
+        storage.save(currentUser.user)
+        storage.save(currentUser.relation(\.user, inverse: \.currentUser))
         
         storage.save(message)
         storage.save(attachment)
@@ -66,7 +37,7 @@ final class SwiftyModelTests: XCTestCase {
             .resolve()
             .compactMap { $0 } ?? []
         
-        
+      
         storage.save(message.relation(\.attachments, option: .append, inverse: \.message))
         storage.save(attachment.relation(\.message, inverse: \.attachments))
         storage.save(user.relation(\.messages, option: .append))
