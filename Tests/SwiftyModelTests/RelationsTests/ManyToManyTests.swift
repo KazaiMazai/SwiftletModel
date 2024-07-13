8f76c1b6a8eec1d14b6f69c1077fbea0a15b01bd
Sergey Kazakov <kazaimazai@gmail.com>
2024-07-13 20:44:24 +0100

    updated tests

--- Tests/SwiftyModelTests/RelationsTests/ManyToManyTests.swift
+++ Tests/SwiftyModelTests/RelationsTests/ManyToManyTests.swift
@@ -26,7 +26,7 @@ final class ManyToManyTests: XCTestCase {
             .related(\.chats)
             .resolve() 
         
-        XCTAssertEqual(Set(bobChats.compactMap { $0?.id }), Set(arrayLiteral: Chat.one.id, Chat.two.id))
+        XCTAssertEqual(bobChats.compactMap { $0?.id }, [Chat.one.id, Chat.two.id])
     }
     
     func test_WhenRelationUpdatedWithInsert_NewRelationsInserted() {
@@ -44,8 +44,8 @@ final class ManyToManyTests: XCTestCase {
         
         let expectedChatUsers = [User.bob.id, User.alice.id, User.tom.id, User.john.id, User.michael.id]
         
-        XCTAssertEqual(Set(chatUsers.compactMap { $0?.id }),
-                       Set(expectedChatUsers))
+        XCTAssertEqual(chatUsers.compactMap { $0?.id },
+                       expectedChatUsers)
     }
     
     func test_WhenDirectReplaced_InverseIsUpdated() {
