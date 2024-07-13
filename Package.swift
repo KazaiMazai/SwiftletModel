8c4356c5f0ec784dd21ad7670dd65819e4fee589
Sergey Kazakov <kazaimazai@gmail.com>
2024-07-13 20:42:48 +0100

    added swift-collections dependency

--- Package.swift
+++ Package.swift
@@ -6,21 +6,22 @@ import PackageDescription
 let package = Package(
     name: "SwiftyModel",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftyModel",
             targets: ["SwiftyModel"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+             url: "https://github.com/apple/swift-collections.git",
+             .upToNextMajor(from: "1.1.0")
+           )
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftyModel",
-            dependencies: []),
+            dependencies: [
+                .product(name: "Collections", package: "swift-collections")
+            ]),
         .testTarget(
             name: "SwiftyModelTests",
             dependencies: ["SwiftyModel"]),
