aeb88504aef0a9d8a9702ec0f13800c10c0ae83c
Sergey Kazakov <kazaimazai@gmail.com>
2024-07-23 16:56:12 +0100

    throwing save method

--- Sources/SwiftletData/EntityModel.swift
+++ Sources/SwiftletData/EntityModel.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public protocol Storable {
-    func save(_ repository: inout Repository)
+    func save(_ repository: inout Repository) throws
 }
 
 public protocol EntityModel: Storable {
