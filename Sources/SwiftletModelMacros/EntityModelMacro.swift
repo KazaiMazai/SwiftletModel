56084f6f21c9e7d34022947a33e03f1672654130
Sergey Kazakov <kazaimazai@gmail.com>
2024-08-03 22:26:37 +0100

    updated marcos implementation

--- Sources/SwiftletModelMacros/EntityModelMacro.swift
+++ Sources/SwiftletModelMacros/EntityModelMacro.swift
@@ -19,10 +19,32 @@ import SwiftSyntaxMacros
 public struct StorableEntityMacro: ExtensionMacro {
     public static func expansion(of node: SwiftSyntax.AttributeSyntax, attachedTo declaration: some SwiftSyntax.DeclGroupSyntax, providingExtensionsOf type: some SwiftSyntax.TypeSyntaxProtocol, conformingTo protocols: [SwiftSyntax.TypeSyntax], in context: some SwiftSyntaxMacros.MacroExpansionContext) throws -> [SwiftSyntax.ExtensionDeclSyntax] {
         
+        let attributes = declaration.memberBlock.members
+            .compactMap { $0.decl.as(VariableDeclSyntax.self) }
+            .compactMap { extractRelationPropertyWrappersAttributes(from: $0) }
+        
+        let saveAll = attributes
+            .map { "try save(\($0.keyPathAttributes.attribute), to: &context)" }
+            .joined(separator: "\n")
+        
+        let normalizeAll = attributes
+            .map { "$\($0.propertyName).normalize()" }
+            .joined(separator: "\n")
+        
+        let syntax = try ExtensionDeclSyntax("""
+        extension \(raw: type) {
+            func saveMe(to context: inout Context) throws {
+                context.insert(self)
+                \(raw: saveAll)
+            }
+        
+            mutating func normalizeMe() {
+               \(raw: normalizeAll)
+            }
+        }
+        """)
 
-        let normalize = try normalizeMe(declaration: declaration, type: type)
-        let save = try saveMe(declaration: declaration, type: type)
-        return [normalize, save].compactMap { $0 }
+        return [syntax]
     }
      
     static func relationAttribute(from list: AttributeListSyntax) -> Bool {
@@ -121,6 +143,120 @@ public struct StorableEntityMacro: ExtensionMacro {
         
         return syntax
     }
-     
     
+    static func save(declaration: some SwiftSyntax.DeclGroupSyntax,
+                     type: some SwiftSyntax.TypeSyntaxProtocol) throws -> SwiftSyntax.ExtensionDeclSyntax? {
+        
+        let attributes = declaration.memberBlock.members
+            .compactMap { $0.decl.as(VariableDeclSyntax.self) }
+            .compactMap { extractRelationPropertyWrappersAttributes(from: $0) }
+        
+        let saveAll = attributes
+            .map { "try save(\($0.keyPathAttributes.attribute), to: &context)" }
+            .joined(separator: "\n")
+        
+        let normalizeAll = attributes
+            .map { "$\($0.propertyName).normalize()" }
+            .joined(separator: "\n")
+        
+        let syntax = try ExtensionDeclSyntax("""
+        extension \(raw: type) {
+            func saveMe(to context: inout Context) throws {
+                context.insert(self)
+                \(raw: saveAll)
+            }
+        
+            mutating func normalizeMe() {
+               \(raw: normalizeAll)
+            }
+        }
+        """)
+        
+        return syntax
+    }
+    
+    static func extractRelationPropertyWrappersAttributes(from vars: VariableDeclSyntax) -> RelationPropertyWrapperAttributes? {
+        for attribute in vars.attributes {
+            guard let customAttribute = attribute.as(AttributeSyntax.self),
+                let identifierTypeSyntax = customAttribute.attributeName.as(IdentifierTypeSyntax.self),
+                let wrapperType = RelationPropertyWrapperAttributes.WrapperType(rawValue: identifierTypeSyntax.name.text)
+            else {
+                continue
+            }
+            
+            guard let binding = vars.bindings.first(where: { $0.pattern.as(IdentifierPatternSyntax.self)?.identifier.text != nil }),
+                  let property = binding.pattern.as(IdentifierPatternSyntax.self)?.identifier.text
+            else {
+                return nil
+            }
+            
+            guard let argumentList = customAttribute.arguments?.as(LabeledExprListSyntax.self) else {
+                return RelationPropertyWrapperAttributes(
+                    relationWrapperType: wrapperType,
+                    propertyName: property,
+                    keyPathAttributes: .init(property: property)
+                )
+            }
+            
+            return RelationPropertyWrapperAttributes(
+                relationWrapperType: wrapperType,
+                propertyName: property,
+                keyPathAttributes: .init(mutual: argumentList.map { argument in
+                    let label = argument.label.map { "\($0)" }
+                    let expression = "\(argument.expression)"
+                    return (label, expression)
+                })
+            )
+        }
+        return nil
+    }
+}
+
+
+struct RelationPropertyWrapperAttributes {
+    enum WrapperType: String, CaseIterable {
+        case hasMany = "HasMany"
+        case hasOne = "HasOne"
+        case belongsTo = "BelongsTo"
+        
+        var title: String {
+            rawValue
+        }
+        
+        static let allCasesTitleSet: Set<String> = {
+            Set(Self.allCases.map { $0.title })
+        }()
+    }
+    
+    enum KeyPathAttributes {
+        case mutual(String)
+        case oneWay(String)
+        
+        init(mutual: [(label: String?, expression: String)]) {
+            let keyPathAttributes = mutual.map {
+                let label = $0.label.map { "\($0): "} ?? ""
+                let expression = "\($0.expression)".replacingOccurrences(of: "\\.", with: "\\.$")
+                return "\(label)\(expression)"
+            }
+            .joined(separator: ",")
+            
+            self = .mutual(keyPathAttributes)
+        }
+        
+        init(property: String) {
+            self = .oneWay("\\.$\(property)")
+        }
+        
+        var attribute: String {
+            switch self {
+            case .mutual(let value), .oneWay(let value):
+                return value
+            }
+        }
+        
+    }
+    
+    let relationWrapperType: WrapperType
+    let propertyName: String
+    let keyPathAttributes: KeyPathAttributes
 }
