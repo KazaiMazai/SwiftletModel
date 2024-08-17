5f262d25457b3c738e062099d9b9ae9e544933fb
Sergey Kazakov <kazaimazai@gmail.com>
2024-08-17 22:06:59 +0100

    index model methods implementation: filter, add, remove

--- Sources/SwiftletModel/Model/IndexModel.swift
+++ Sources/SwiftletModel/Model/IndexModel.swift
@@ -11,41 +11,76 @@ import Collections
 
 @EntityModel
 struct IndexModel<Entity: EntityModelProtocol, Value: Comparable> {
+    typealias IndexMap = Map<Value, OrderedSet<Entity.ID>>
     var id: String { name }
     
     let name: String
     
-    var index: Map<Value, OrderedSet<Entity.ID>> = [:]
-    var values: [Entity.ID: Value] = [:]
+    private var indexMap: IndexMap = [:]
+    private var values: [Entity.ID: Value] = [:]
     
-    @Relationship
-    var entities: [Entity]? = nil
+    init(name: String) {
+        self.name = name
+    }
     
-    
-    
-    mutating func update(_ entity: Entity, value: Value) {
+    var ids: [Entity.ID] { indexMap.flatMap { $0.1.elements } }
+}
+
+extension IndexModel {
+    mutating func add(_ entity: Entity, value: Value) {
         let existingValue = values[entity.id]
         
         guard existingValue != value else {
             return
         }
         
-        if let existingValue, var ids = index[existingValue] {
+        if let existingValue, var ids = indexMap[existingValue] {
             ids.remove(entity.id)
-            index[existingValue] = ids.isEmpty ? nil : ids
+            indexMap[existingValue] = ids.isEmpty ? nil : ids
         }
         
-        guard var ids = index[value] else {
-            index[value] = OrderedSet(arrayLiteral: entity.id)
+        guard var ids = indexMap[value] else {
+            indexMap[value] = OrderedSet(arrayLiteral: entity.id)
             values[entity.id] = value
-            $entities = .ids([entity.id])
             return
         }
         
         ids.append(entity.id)
-        index[value] = ids
-        $entities = .ids(ids.elements)
+        indexMap[value] = ids
         values[entity.id] = value
     }
+    
+    mutating func remove(_ entity: Entity) {
+        guard let value = values[entity.id] else {
+            return
+        }
+        
+        guard var ids = indexMap[value] else {
+            return
+        }
+        
+        ids.remove(entity.id)
+        indexMap[value] = ids
+    }
 }
+
+extension IndexModel {
+    func filter(_ value: Value) -> [Entity.ID] {
+        indexMap[value]?.elements ?? []
+    }
+    
+    func filter(range: Range<Value>) -> [Entity.ID] {
+        indexMap
+            .submap(from: range.lowerBound, to: range.upperBound)
+            .map { $1.elements }
+            .flatMap { $0 }
+    }
+    
+    func grouped() -> [Value: [Entity.ID]] where Value: Hashable {
+        Dictionary(indexMap.map { ($0, $1.elements) },
+                   uniquingKeysWith: { $1 })
+            
+    }
+}
+ 
  
