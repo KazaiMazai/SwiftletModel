5778bfb75d2f7d747a5ee8f4856c711f67254926
Sergey Kazakov <kazaimazai@gmail.com>
2024-09-11 01:01:41 +0100

    added access attributes enum

---
+++ Sources/SwiftletModelMacros/AccessAttributes.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sergey Kazakov on 11/09/2024.
+//
+
+import Foundation
+
+enum AccessAttribute: String {
+    case `private`
+    case `fileprivate`
+    case `internal`
+    case `public`
+    case `open`
+    case missingAttribute = ""
+    
+    var name: String {
+        rawValue
+    }
+}
