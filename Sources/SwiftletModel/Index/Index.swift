72e94075987f7db8874c4ca0c17f752c92c8ecc4
Sergey Kazakov <kazaimazai@gmail.com>
2025-03-09 00:47:19 +0000

    index updates

---
+++ Sources/SwiftletModel/Index/Index.swift
@@ -0,0 +1,40 @@
+//
+//  File.swift
+//  SwiftletModel
+//
+//  Created by Sergey Kazakov on 08/03/2025.
+//
+
+import Foundation
+
+@propertyWrapper
+public struct Index<Entity, T: Comparable>: Sendable, Codable {
+    public var wrappedValue: T.Type {
+        T.self
+    }
+    
+    public init(_ keyPath: KeyPath<Entity, T>) {
+    }
+
+    public init<T0, T1>(
+        _ kp0: KeyPath<Entity, T0>,
+        _ kp1: KeyPath<Entity, T1>) where T == Pair<T0, T1> {
+
+    }
+
+    public init<T0, T1, T2>(
+        _ kp0: KeyPath<Entity, T0>,
+        _ kp1: KeyPath<Entity, T1>,
+        _ kp2: KeyPath<Entity, T2>) where T == Triplet<T0, T1, T2> {
+
+    }
+
+    public init<T0, T1, T2, T3>(
+        _ kp0: KeyPath<Entity, T0>,
+        _ kp1: KeyPath<Entity, T1>,
+        _ kp2: KeyPath<Entity, T2>,
+        _ kp3: KeyPath<Entity, T3>) where T == Quadruple<T0, T1, T2, T3> {
+        
+    }
+}
+
