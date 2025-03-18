419022728794128d704a7fe4a87b18268e717348
Sergey Kazakov <kazaimazai@gmail.com>
2025-03-19 02:50:02 +0300

    updated models for testing

--- Tests/SwiftletModelTests/ModelsForTesting.swift
+++ Tests/SwiftletModelTests/ModelsForTesting.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 extension User {
-    static let bob = User(id: "1", name: "Bob")
-    static let alice = User(id: "2", name: "Alice")
-    static let john = User(id: "3", name: "John")
-    static let michael = User(id: "4", name: "Michael")
-    static let tom = User(id: "5", name: "Tom")
+    static let bob = User(id: "1", name: "Bob", username: "@bob", email: "bob@gmail.com")
+    static let alice = User(id: "2", name: "Alice", username: "@alice", email: "alice@gmail.com")
+    static let john = User(id: "3", name: "John", username: "@john", email: "john@gmail.com")
+    static let michael = User(id: "4", name: "Michael", username: "@michael", email: "michael@gmail.com")
+    static let tom = User(id: "5", name: "Tom", username: "@tom", email: "tom@gmail.com")
 }
 
 extension Attachment {
