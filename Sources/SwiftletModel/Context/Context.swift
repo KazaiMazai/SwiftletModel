730e19a3547b7b7bb13204b0f0caafc93833961b
Sergey Kazakov <kazaimazai@gmail.com>
2025-03-28 16:39:06 +0000

    refactoring

--- Sources/SwiftletModel/Context/Context.swift
+++ Sources/SwiftletModel/Context/Context.swift
@@ -77,43 +77,4 @@ extension Context {
         relationsRepository.getChildren(for: type, relationName: relationName, id: id)
     }
 }
-//
-//extension Context {
-//    func index<Entity, T, Index>(
-//        _ keyPath: KeyPath<Entity, T>) -> Index?
-//    where
-//    Index: EntityModelProtocol,
-//    Index.ID == String {
-//        entitiesRepository.find(.indexName(keyPath))
-//    }
-//    
-//    func index<Entity, T0, T1, Index>(
-//        _ kp0: KeyPath<Entity, T0>,
-//        _ kp1: KeyPath<Entity, T1>) -> Index?
-//    where
-//    Index: EntityModelProtocol,
-//    Index.ID == String {
-//        entitiesRepository.find(.indexName(kp0, kp1))
-//    }
-//    
-//    func index<Entity, T0, T1, T2, Index>(
-//        _ kp0: KeyPath<Entity, T0>,
-//        _ kp1: KeyPath<Entity, T1>,
-//        _ kp2: KeyPath<Entity, T2>) -> Index?
-//    where
-//     Index: EntityModelProtocol,
-//    Index.ID == String {
-//        entitiesRepository.find(.indexName(kp0, kp1, kp2))
-//    }
-//    
-//    func index<Entity, T0, T1, T2, T3, Index>(
-//        _ kp0: KeyPath<Entity, T0>,
-//        _ kp1: KeyPath<Entity, T1>,
-//        _ kp2: KeyPath<Entity, T2>,
-//        _ kp3: KeyPath<Entity, T3>) -> Index?
-//    where
-//     Index: EntityModelProtocol,
-//    Index.ID == String {
-//        entitiesRepository.find(.indexName(kp0, kp1, kp2, kp3))
-//    }
-//}
+
