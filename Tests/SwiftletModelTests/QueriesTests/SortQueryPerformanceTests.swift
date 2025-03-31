77cb7c14fbf67b9856ef0b4fa083f05c530034be
Sergey Kazakov <kazaimazai@gmail.com>
2025-03-31 23:14:16 +0100

    updated tests

--- Tests/SwiftletModelTests/QueriesTests/SortQueryPerformanceTests.swift
+++ Tests/SwiftletModelTests/QueriesTests/SortQueryPerformanceTests.swift
@@ -11,7 +11,7 @@ import XCTest
 
 
 class SortQueryPerformanceTests: XCTestCase {
-    var count: Int  { 10000 }
+    var count: Int  { 1000 }
     
     lazy var notIndexedModels = {
         (0..<count).map { idx in TestModel(id: "\(idx)", value: idx) }
@@ -22,50 +22,33 @@ class SortQueryPerformanceTests: XCTestCase {
     }()
 
     func test_NoIndexPerformance() throws {
-        let models = notIndexedModels
+        let models = notIndexedModels.shuffled()
        
         var context = Context()
         try models.forEach {  try $0.save(to: &context) }
-        
+        let queries = TestModel.query(in: context)
         measure {
-            let _ = TestModel
-                .query(in: context)
+            let _ = queries
                 .sorted(by: \.value)
                 .resolve()
         }
     }
     
-    func test_NoIndexRawPerformance() throws {
-        let models = notIndexedModels
-       
-        var context = Context()
-        try models.forEach {  try $0.save(to: &context) }
-        
-        measure {
-            let _ = TestModel
-                .query(in: context)
-                .resolve()
-                .shuffled()
-                .sorted { $0.value < $1.value }
-        }
-    }
-    
     func test_IndexPerformance() throws {
-        let models = indexedModels
+        let models = indexedModels.shuffled()
        
         var context = Context()
         try models.forEach {  try $0.save(to: &context) }
-        
+        let queries = TestIndexedModel.query(in: context)
         measure {
-            let _ = TestIndexedModel
-                .query(in: context)
+            let _ = queries
                 .sorted(by: \.value)
                 .resolve()
         }
     }
     
     func test_IndexDescPerformance() throws {
-        let models = indexedModels
+        let models = indexedModels.shuffled()
        
         var context = Context()
         try models.forEach {  try $0.save(to: &context) }
@@ -76,21 +59,19 @@ class SortQueryPerformanceTests: XCTestCase {
                 .sorted(by: \.value.desc)
                 .resolve()
         }
-         
     }
     
     func test_NoIndexSavePerformance() throws {
-        let models = notIndexedModels
+        let models = notIndexedModels.shuffled()
        
         var context = Context()
         measure {
             try! models.forEach { try $0.save(to: &context) }
         }
-        
     }
     
     func test_IndexBuildSavePerformance() throws {
-        let models = indexedModels
+        let models = indexedModels.shuffled()
        
         var context = Context()
         measure {
