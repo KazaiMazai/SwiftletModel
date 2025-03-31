dfe704bd87e4580bb2280ed78311bc19dded728e
Sergey Kazakov <kazaimazai@gmail.com>
2025-03-31 14:43:58 +0100

    sorting tests

--- Tests/SwiftletModelTests/QueriesTests/SortQueryPerformanceTests.swift
+++ Tests/SwiftletModelTests/QueriesTests/SortQueryPerformanceTests.swift
@@ -11,81 +11,91 @@ import XCTest
 
 
 class SortQueryPerformanceTests: XCTestCase {
-    func testSortNoIndexPerformance() throws {
-        let count = 1000
-        let models = (0..<count).map { idx in
-            TestModel(id: "\(idx)", value: idx)
-        }
+    var count: Int  { 10000 }
+    
+    lazy var notIndexedModels = {
+        (0..<count).map { idx in TestModel(id: "\(idx)", value: idx) }
+    }()
+    
+    lazy var indexedModels = {
+        (0..<count).map { idx in TestIndexedModel(id: "\(idx)", value: idx) }
+    }()
+
+    func test_NoIndexPerformance() throws {
+        let models = notIndexedModels
        
         var context = Context()
         try models.forEach {  try $0.save(to: &context) }
         
         measure {
-            let _ = TestModel.query(in: context)
+            let _ = TestModel
+                .query(in: context)
                 .sorted(by: \.value)
                 .resolve()
         }
-        
     }
     
-    func testSortIndexedPerformance() throws {
-        let count = 1000
-        let models = (0..<count).map { idx in
-            TestIndexedModel(id: "\(idx)", value: idx)
-        }
+    func test_NoIndexRawPerformance() throws {
+        let models = notIndexedModels
        
         var context = Context()
         try models.forEach {  try $0.save(to: &context) }
         
         measure {
-            let _ = TestIndexedModel.query(in: context)
+            let _ = TestModel
+                .query(in: context)
+                .resolve()
+                .shuffled()
+                .sorted { $0.value < $1.value }
+        }
+    }
+    
+    func test_IndexPerformance() throws {
+        let models = indexedModels
+       
+        var context = Context()
+        try models.forEach {  try $0.save(to: &context) }
+        
+        measure {
+            let _ = TestIndexedModel
+                .query(in: context)
                 .sorted(by: \.value)
                 .resolve()
         }
-         
     }
     
-    func testSortIndexedDescPerformance() throws {
-        let count = 1000
-        let models = (0..<count).map { idx in
-            TestIndexedModel(id: "\(idx)", value: idx)
-        }
+    func test_IndexDescPerformance() throws {
+        let models = indexedModels
        
         var context = Context()
         try models.forEach {  try $0.save(to: &context) }
         
         measure {
-            let _ = TestIndexedModel.query(in: context)
+            let _ = TestIndexedModel
+                .query(in: context)
                 .sorted(by: \.value.desc)
                 .resolve()
         }
          
     }
     
-    func testNoIndexBuildPerformance() throws {
-        let count = 1000
-        let models = (0..<count).map { idx in
-            TestModel(id: "\(idx)", value: idx)
-        }
+    func test_NoIndexSavePerformance() throws {
+        let models = notIndexedModels
        
         var context = Context()
         measure {
-            try! models.forEach {  try $0.save(to: &context) }
+            try! models.forEach { try $0.save(to: &context) }
         }
         
     }
     
-    func testIndexBuildPerformance() throws {
-        let count = 1000
-        let models = (0..<count).map { idx in
-            TestIndexedModel(id: "\(idx)", value: idx)
-        }
+    func test_IndexBuildSavePerformance() throws {
+        let models = indexedModels
        
         var context = Context()
         measure {
-            try! models.forEach {  try $0.save(to: &context) }
+            try! models.forEach { try $0.save(to: &context) }
         }
-         
     }
 }
 
