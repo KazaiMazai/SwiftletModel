5962eaa8ecec471919996a8bd11908e9af804004
Sergey Kazakov <kazaimazai@gmail.com>
2025-04-06 13:45:41 +0100

    added filter text tests

---
+++ Tests/SwiftletModelTests/QueriesTests/FilterTextQueryTests.swift
@@ -0,0 +1,104 @@
+//
+//  File.swift
+//  SwiftletModel
+//
+//  Created by Sergey Kazakov on 02/04/2025.
+//
+
+@testable import SwiftletModel
+import Foundation
+import XCTest
+
+final class FilterTextQueryTests: XCTestCase {
+    var context = Context()
+    
+    lazy var notIndexedModels = {
+        TestingModels.StringNotIndexed.shuffled()
+    }()
+
+    lazy var indexedModels = {
+        TestingModels.StringFullTextIndexed.shuffled()
+    }()
+    
+    override func setUp() async throws {
+        context = Context()
+        try notIndexedModels
+            .forEach { try $0.save(to: &context) }
+        
+        try indexedModels
+            .forEach { try $0.save(to: &context) }
+    }
+    
+    func test_WhenContainsFilterNoIndex_ThenEqualPlainFitlering() throws {
+        let expected = notIndexedModels
+            .filter { $0.text.contains("ananas") }
+       
+        let filterResult = TestingModels.StringNotIndexed
+            .filter(.string(\.text, contains: "ananas"), in: context)
+            .resolve()
+        
+        XCTAssertEqual(Set(filterResult.map { $0.id }),
+                       Set(expected.map { $0.id }))
+    }
+    
+    func test_WhenContainsFilterIndexed_ThenEqualPlainFiltering() throws {
+        let expected = notIndexedModels
+            .filter { $0.text.contains("ananas") }
+       
+        let filterResult = TestingModels.StringFullTextIndexed
+            .filter(.string(\.text, contains: "ananas"), in: context)
+            .resolve()
+        
+        XCTAssertEqual(Set(filterResult.map { $0.id }),
+                       Set(expected.map { $0.id }))
+    }
+
+    func test_WhenStartsWithFilterNoIndex_ThenEqualPlainFiltering() throws {
+        let expected = notIndexedModels
+            .filter { $0.text.hasPrefix("bananas") }
+       
+        let filterResult = TestingModels.StringNotIndexed
+            .filter(.string(\.text, startsWith: "bananas"), in: context)
+            .resolve()
+        
+        XCTAssertEqual(Set(filterResult.map { $0.id }),
+                       Set(expected.map { $0.id }))
+    }
+    
+    func test_WhenStartsWithFilterIndexed_ThenEqualPlainFiltering() throws {
+        let expected = notIndexedModels
+            .filter { $0.text.starts(with: "bananas") }
+       
+        let filterResult = TestingModels.StringFullTextIndexed
+            .filter(.string(\.text, startsWith: "bananas"), in: context)
+            .resolve()
+        
+        XCTAssertEqual(Set(filterResult.map { $0.id }),
+                       Set(expected.map { $0.id }))
+    }
+    
+    func test_WhenEndsWithFilterNoIndex_ThenEqualPlainFiltering() throws {
+        let expected = notIndexedModels
+            .filter { $0.text.hasSuffix("bananas") }
+       
+        let filterResult = TestingModels.StringNotIndexed
+            .filter(.string(\.text, endsWith: "bananas"), in: context)
+            .resolve()
+        
+        XCTAssertEqual(Set(filterResult.map { $0.id }),
+                       Set(expected.map { $0.id }))
+    }
+    
+    func test_WhenEndsWithFilterIndexed_ThenEqualPlainFiltering() throws {
+        let expected = notIndexedModels
+            .filter { $0.text.hasSuffix("bananas") }
+       
+        let filterResult = TestingModels.StringFullTextIndexed
+            .filter(.string(\.text, endsWith: "bananas"), in: context)
+            .resolve()
+        
+        XCTAssertEqual(Set(filterResult.map { $0.id }),
+                       Set(expected.map { $0.id }))
+    }
+
+}
