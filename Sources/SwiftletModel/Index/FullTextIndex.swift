035c429033cbb2d64362e638706b922a6ce378f4
Sergey Kazakov <kazaimazai@gmail.com>
2025-04-06 19:53:16 +0100

    updated fulltext search index property wrapper definitions

--- Sources/SwiftletModel/Index/FullTextIndex.swift
+++ Sources/SwiftletModel/Index/FullTextIndex.swift
@@ -14,25 +14,7 @@ public struct FullTextIndex<Entity: EntityModelProtocol>: Sendable, Codable {
         self
     }
     
-    public init(_ kp0: KeyPath<Entity, String>) {
+    public init(_ keypaths: KeyPath<Entity, String>...) {
         
-    }
-    
-    public init(
-        _ kp0: KeyPath<Entity, String>,
-        _ kp1: KeyPath<Entity, String>) {
-    }
-    
-    public init(
-        _ kp0: KeyPath<Entity, String>,
-        _ kp1: KeyPath<Entity, String>,
-        _ kp2: KeyPath<Entity, String>) {
-    }
-    
-    public init(
-        _ kp0: KeyPath<Entity, String>,
-        _ kp1: KeyPath<Entity, String>,
-        _ kp2: KeyPath<Entity, String>,
-        _ kp3: KeyPath<Entity, String>) {
     }
 }
