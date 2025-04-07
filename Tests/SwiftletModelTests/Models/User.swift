379504c3952335376fffefc83abd4a7ddc230c4b
Serge Kazakov <kazaimazai@gmail.com>
2025-04-07 17:01:09 +0100

    Minor test fix (#70)

--- Tests/SwiftletModelTests/Models/User.swift
+++ Tests/SwiftletModelTests/Models/User.swift
@@ -31,13 +31,11 @@ extension User {
     }
 }
 
-
+@EntityModel
 struct User: Codable, Sendable {
     @Unique<Self>(\.username, collisions: .upsert) static var uniqueUsername
     @Unique<Self>(\.email, collisions: .throw) static var uniqueEmail
-    
     @Unique<Self>(\.isCurrent, collisions: .updateCurrentUser) static var currentUserIndex
-    
     
     let id: String
     private(set) var name: String?
@@ -54,75 +52,6 @@ struct User: Codable, Sendable {
     @Relationship(inverse: \.admins)
     var adminOf: [Chat]?
 }
-
-extension User: EntityModelProtocol {
-    func save(to context: inout Context, options: MergeStrategy<Self> = .default) throws {
-        try willSave(to: &context)
-        try updateUniqueIndex(\.username, collisions: .upsert, in: &context)
-        try updateUniqueIndex(\.email, collisions: .throw, in: &context)
-        try updateUniqueIndex(\.isCurrent, collisions: .updateCurrentUser, in: &context)
-        
-        
-        context.insert(self, options: options)
-        try save(\.$chats, inverse: \.$users, to: &context)
-        try save(\.$adminOf, inverse: \.$admins, to: &context)
-        try didSave(to: &context)
-    }
-    func delete(from context: inout Context) throws {
-        try willDelete(from: &context)
-        try removeFromUniqueIndex(\.username, in: &context)
-        try removeFromUniqueIndex(\.email, in: &context)
-        try removeFromUniqueIndex(\.isCurrent, in: &context)
-        
-        context.remove(Self.self, id: id)
-        detach(\.$chats, inverse: \.$users, in: &context)
-        detach(\.$adminOf, inverse: \.$admins, in: &context)
-        try didDelete(from: &context)
-    }
-    mutating func normalize() {
-        $chats.normalize()
-        $adminOf.normalize()
-    }
-    
-    static func nestedQueryModifier(_ query: Query<Self>, nested: [Nested]) -> Query<Self> {
-        guard let relation = nested.first else {
-            return query
-        }
-        
-        let next = Array(nested.dropFirst())
-        return switch relation {
-        case .ids:
-            query
-                .id(\.$chats)
-                .id(\.$adminOf)
-        case .fragments:
-            query
-                .fragment(\.$chats) {
-                    $0.with(next)
-                }
-                .fragment(\.$adminOf) {
-                    $0.with(next)
-                }
-        case .entities:
-            query
-                .with(\.$chats) {
-                    $0.with(next)
-                }
-                .with(\.$adminOf) {
-                    $0.with(next)
-                }
-        }
-    }
-    
-    static var patch: MergeStrategy<Self> {
-        MergeStrategy(
-            .patch(\.name),
-            .patch(\.avatar),
-            .patch(\.profile)
-        )
-    }
-}
-
  
 extension CollisionResolver where Entity == User {
     static var updateCurrentUser: Self {
