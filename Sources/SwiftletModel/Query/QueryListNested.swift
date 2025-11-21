0daacfa16c0dbed7f1a74d54db93124dfd233adb
Sergey Kazakov <kazaimazai@gmail.com>
2025-11-21 18:40:13 +0000

    lazy nested queries

--- Sources/SwiftletModel/Query/QueryListNested.swift
+++ Sources/SwiftletModel/Query/QueryListNested.swift
@@ -11,7 +11,7 @@ public extension ContextQuery where Result == [Query<Entity>], Key == Void {
         nested: @escaping QueryModifier<Child> = { $0 }) -> QueryList<Entity> {
 
         then { _, queries in
-            queries.map { $0.with(keyPath, fragment: false, nested: nested) }
+            queries.map { $0.with(keyPath, fragment: false, lazy: false, nested: nested) }
         }
     }
 
@@ -21,7 +21,7 @@ public extension ContextQuery where Result == [Query<Entity>], Key == Void {
     ) -> QueryList<Entity> {
 
         then { _, queries in
-            queries.map { $0.with(keyPath, slice: false, fragment: false, nested: nested) }
+            queries.map { $0.with(keyPath, slice: false, fragment: false, lazy: false, nested: nested) }
         }
     }
 
@@ -31,7 +31,38 @@ public extension ContextQuery where Result == [Query<Entity>], Key == Void {
     ) -> QueryList<Entity> {
 
         then { _, queries in
-            queries.map { $0.with(keyPath, slice: true, fragment: false, nested: nested) }
+            queries.map { $0.with(keyPath, slice: true, fragment: false, lazy: false, nested: nested) }
+        }
+    }
+}
+
+public extension ContextQuery where Result == [Query<Entity>], Key == Void {
+    func lazy<Child, Directionality, Constraints>(
+        _ keyPath: WritableKeyPath<Entity, ToOneRelation<Child, Directionality, Constraints>>,
+        nested: @escaping QueryModifier<Child> = { $0 }) -> QueryList<Entity> {
+
+        then { _, queries in
+            queries.map { $0.with(keyPath, fragment: false, lazy: true, nested: nested) }
+        }
+    }
+
+    func lazy<Child, Directionality, Constraints>(
+        _ keyPath: WritableKeyPath<Entity, ToManyRelation<Child, Directionality, Constraints>>,
+        nested: @escaping QueryListModifier<Child> = { $0 }
+    ) -> QueryList<Entity> {
+
+        then { _, queries in
+            queries.map { $0.with(keyPath, slice: false, fragment: false, lazy: true, nested: nested) }
+        }
+    }
+
+    func lazy<Child, Directionality, Constraints>(
+        slice keyPath: WritableKeyPath<Entity, ToManyRelation<Child, Directionality, Constraints>>,
+        nested: @escaping QueryListModifier<Child> = { $0 }
+    ) -> QueryList<Entity> {
+
+        then { _, queries in
+            queries.map { $0.with(keyPath, slice: true, fragment: false, lazy: true, nested: nested) }
         }
     }
 }
@@ -45,7 +76,7 @@ public extension ContextQuery where Result == [Query<Entity>], Key == Void {
     ) -> QueryList<Entity> {
 
         then { _, queries in
-            queries.map { $0.with(keyPath, fragment: true, nested: nested) }
+            queries.map { $0.with(keyPath, fragment: true, lazy: false, nested: nested) }
         }
     }
 
@@ -55,7 +86,7 @@ public extension ContextQuery where Result == [Query<Entity>], Key == Void {
     ) -> QueryList<Entity> {
 
         then { _, queries in
-            queries.map { $0.with(keyPath, slice: false, fragment: true, nested: nested) }
+            queries.map { $0.with(keyPath, slice: false, fragment: true, lazy: false, nested: nested) }
         }
     }
 
@@ -65,7 +96,39 @@ public extension ContextQuery where Result == [Query<Entity>], Key == Void {
     ) -> QueryList<Entity> {
 
         then { _, queries in
-            queries.map { $0.with(keyPath, slice: true, fragment: true, nested: nested) }
+            queries.map { $0.with(keyPath, slice: true, fragment: true, lazy: false, nested: nested) }
+        }
+    }
+}
+
+public extension ContextQuery where Result == [Query<Entity>], Key == Void {
+    func lazyFragment<Child, Directionality, Constraints>(
+        _ keyPath: WritableKeyPath<Entity, ToOneRelation<Child, Directionality, Constraints>>,
+        nested: @escaping QueryModifier<Child> = { $0 }
+    ) -> QueryList<Entity> {
+
+        then { _, queries in
+            queries.map { $0.with(keyPath, fragment: true, lazy: true, nested: nested) }
+        }
+    }
+
+    func lazyFragment<Child, Directionality, Constraints>(
+        _ keyPath: WritableKeyPath<Entity, ToManyRelation<Child, Directionality, Constraints>>,
+        nested: @escaping QueryListModifier<Child> = { $0 }
+    ) -> QueryList<Entity> {
+
+        then { _, queries in
+            queries.map { $0.with(keyPath, slice: false, fragment: true, lazy: true, nested: nested) }
+        }
+    }
+
+    func lazyFragment<Child, Directionality, Constraints>(
+        slice keyPath: WritableKeyPath<Entity, ToManyRelation<Child, Directionality, Constraints>>,
+        nested: @escaping QueryListModifier<Child> = { $0 }
+    ) -> QueryList<Entity> {
+
+        then { _, queries in
+            queries.map { $0.with(keyPath, slice: true, fragment: true, lazy: true, nested: nested) }
         }
     }
 }
