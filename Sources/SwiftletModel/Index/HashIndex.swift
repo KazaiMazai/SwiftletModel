b391ec500975ff09f77dca1b64aaf497ed33fc15
Sergey Kazakov <kazaimazai@gmail.com>
2026-02-13 22:25:18 +0000

    added HashIndex property wrapper

---
+++ Sources/SwiftletModel/Index/HashIndex.swift
@@ -0,0 +1,57 @@
+//
+//  File.swift
+//  SwiftletModel
+//
+//  Created by Serge Kazakov on 08/03/2025.
+//
+
+import Foundation
+
+@MainActor
+@propertyWrapper
+public struct HashIndex<Entity: EntityModelProtocol>: Sendable, Codable {
+    public var wrappedValue: HashIndex<Entity>.Type {
+        Self.self
+    }
+
+    public init<T0>(_ kp0: KeyPath<Entity, T0>)
+    where
+    T0: Hashable {
+
+    }
+
+    public init<T0, T1>(
+        _ kp0: KeyPath<Entity, T0>,
+        _ kp1: KeyPath<Entity, T1>)
+    where
+    T0: Hashable,
+    T1: Hashable {
+
+    }
+
+    public init<T0, T1, T2>(
+        _ kp0: KeyPath<Entity, T0>,
+        _ kp1: KeyPath<Entity, T1>,
+        _ kp2: KeyPath<Entity, T2>)
+
+    where
+    T0: Hashable,
+    T1: Hashable,
+    T2: Hashable {
+
+    }
+
+    public init<T0, T1, T2, T3>(
+        _ kp0: KeyPath<Entity, T0>,
+        _ kp1: KeyPath<Entity, T1>,
+        _ kp2: KeyPath<Entity, T2>,
+        _ kp3: KeyPath<Entity, T3>)
+
+    where
+    T0: Hashable,
+    T1: Hashable,
+    T2: Hashable,
+    T3: Hashable {
+
+    }
+}
