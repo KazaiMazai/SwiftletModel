76770d03629a5f0e97c25a06420664967997d6ec
Sergey Kazakov <kazaimazai@gmail.com>
2026-02-14 02:47:30 +0000

    generic model example

--- Tests/SwiftletModelTests/Models/Attachment.swift
+++ Tests/SwiftletModelTests/Models/Attachment.swift
@@ -29,3 +29,24 @@ extension Schema.V1 {
     }
 }
 
+extension Schema.V1 {
+
+    @EntityModel
+    struct GenericAttachment<T: Codable & Sendable & Hashable>: Codable, Sendable {
+        @HashIndex<Self>(\.kind) private var kindIndex
+        
+        let id: String
+        var kind: T
+        
+        @Relationship
+        var message: Message? = .none
+
+        enum Kind: Codable, Hashable {
+            case image(url: URL)
+            case video(url: URL)
+            case file(url: URL)
+            case generic(T)
+        }
+    }
+}
+
